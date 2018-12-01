e633732c0e07e549a172b5344debc6084e3b3f7d
Paul Reiter <reiter.paul@gmail.com>
2018-12-01 20:18:09 +0100

    Solve part two of day one

--- 01.py
+++ 01.py
@@ -3,6 +3,8 @@
 2018-12-01 19:51:26
 @author: Paul Reiter
 """
+from itertools import cycle
+
 
 if __name__ == '__main__':
     with open('01_input.txt', 'r') as stream:
@@ -13,3 +15,13 @@ if __name__ == '__main__':
     for frequency_change in frequency_changes:
         current_frequency += frequency_change
     print(current_frequency)
+
+    frequencies = set()
+    current_frequency = 0
+    for frequency_change in cycle(frequency_changes):
+        current_frequency += frequency_change
+        if current_frequency not in frequencies:
+            frequencies.add(current_frequency)
+        else:
+            print(current_frequency)
+            break

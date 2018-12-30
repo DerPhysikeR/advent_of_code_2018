884c31aedf3316e3c36fb47e0b8a5cd2ab743aa5
Paul Reiter <reiter.paul@gmail.com>
2018-12-30 22:06:35 +0100

    Solve part 1 of day 15

---
+++ day_15.py
@@ -0,0 +1,213 @@
+#!/usr/bin/env python3
+"""
+2018-12-28 19:15:21
+@author: Paul Reiter
+"""
+from collections import namedtuple, defaultdict
+from operator import attrgetter
+
+Point = namedtuple('Point', 'x y')
+
+
+class Unit:
+
+    def __init__(self, hp=200, attack_power=3):
+        self.hp = 200
+        self.attack_power = attack_power
+
+    def __repr__(self):
+        return f'Unit(hp={self.hp}, ap={self.attack_power})'
+
+
+class Game:
+
+    def __init__(self, string):
+        positions = position_of_symbols_in_string(string)
+        self.walls = positions['#']
+        self.elves = {position: Unit() for position in positions['E']}
+        self.goblins = {position: Unit() for position in positions['G']}
+        self.rounds_completed = 0
+
+    def __str__(self):
+        elf_dict = {position: 'E' for position in self.elves}
+        goblin_dict = {position: 'G' for position in self.goblins}
+        wall_dict = {position: '#' for position in self.walls}
+        symbol_dict = {**elf_dict, **goblin_dict, **wall_dict}
+        bounding_box = get_bounding_box(symbol_dict)
+        result = [symbol_dict.get(pos, '.') for pos in
+                  grid_generator(*bounding_box)]
+        width = bounding_box[1].x - bounding_box[0].x + 1
+        return '\n'.join(''.join(line) for line in zip(*[iter(result)]*width))
+
+    @classmethod
+    def from_file(cls, filepath):
+        with open(filepath, 'r') as stream:
+            return cls(stream.read())
+
+    @property
+    def units(self):
+        return {**self.elves, **self.goblins}
+
+    def next_unit(self):
+        for position in sort_in_reading_order(self.units):
+            try:
+                yield position, self.units[position]
+            except KeyError:
+                continue
+
+    def get_teams(self, position):
+        if position in self.elves:
+            return self.elves, self.goblins
+        return self.goblins, self.elves
+
+    def step(self, position, unit):
+        allies, enemies = self.get_teams(position)
+        obstacles = set().union(self.walls, allies)
+        new_position = move_towards(position, enemies, obstacles)
+        allies = update_dict(allies, position, new_position)
+        self.fight(new_position, unit, enemies)
+
+    def run_round(self):
+        for position, unit in self.next_unit():
+            if not self.elves or not self.goblins:
+                break
+            self.step(position, unit)
+        else:
+            self.rounds_completed += 1
+
+    def run(self, rounds=None):
+        if not rounds:
+            while self.elves and self.goblins:
+                self.run_round()
+        else:
+            for _ in range(rounds):
+                self.run_round()
+
+    def fight(self, position, unit, enemies):
+        intersection = get_neighbors(position).intersection(enemies)
+        if intersection:
+            min_hp = min(enemies[pos].hp for pos in intersection)
+            enemy_positions = {pos for pos in intersection
+                               if enemies[pos].hp == min_hp}
+            enemy_position = first_in_reading_order(enemy_positions)
+            pos, enemy = enemy_position, enemies[enemy_position]
+            enemy.hp -= unit.attack_power
+            if enemy.hp <= 0:
+                del enemies[pos]
+
+    def outcome(self):
+        return (sum(unit.hp for unit in self.units.values()) *
+                self.rounds_completed)
+
+
+def position_of_symbols_in_string(string):
+    """Return dict of the form {symbol1: {Point(), Point()}, symbol2: {...}}"""
+    positions = defaultdict(set)
+    lines = [line.strip() for line in string.split('\n')]
+    for y, line in enumerate(lines):
+        for x, letter in enumerate(line):
+            positions[letter].add(Point(x, y))
+    return dict(positions)
+
+
+def get_bounding_box(points):
+    """Returns Point(minx, miny), Point(maxx,maxy) for `points`"""
+    minx = min(point.x for point in points)
+    maxx = max(point.x for point in points)
+    miny = min(point.y for point in points)
+    maxy = max(point.y for point in points)
+    return Point(minx, miny), Point(maxx, maxy)
+
+
+def grid_generator(minpoint, maxpoint):
+    """Generator that iterates rowwise over all points inside box defined by
+    `minpoint` and `maxpoint`."""
+    for y in range(minpoint.y, maxpoint.y+1):
+        for x in range(minpoint.x, maxpoint.x+1):
+            yield Point(x, y)
+
+
+def sort_in_reading_order(points):
+    """Returns list of points sorted in reading order"""
+    return sorted(points, key=attrgetter('y', 'x'))
+
+
+def first_in_reading_order(points):
+    """Returns first point in `points` in reading order"""
+    return sort_in_reading_order(points)[0]
+
+
+def update_dict(dictionary, old, new):
+    if new is not None:
+        value = dictionary[old]
+        del dictionary[old]
+        dictionary[new] = value
+    return dictionary
+
+
+def get_neighbors(point, excluding=None):
+    """Returns the orthogonal neighbors of `point` excluding points in
+    `excluding`."""
+    x, y = point
+    neighbor_set = set((Point(x+1, y), Point(x, y-1),
+                        Point(x-1, y), Point(x, y+1)))
+    excluding = set() if excluding is None else set(excluding)
+    return neighbor_set.difference(excluding)
+
+
+def get_all_neighbors(points, excluding=None):
+    """Returns all orthogonal get_neighbors of set of `points` excluding points
+    in `excluding`."""
+    all_neighbors = set().union(*(get_neighbors(point) for point in points))
+    excluding = set() if excluding is None else set(excluding)
+    return all_neighbors.difference(excluding)
+
+
+def neighbor_generator(point, obstacles=None):
+    """Yields sets of points with distance 0, 1, 2, ... from `point`"""
+    exclude = set() if obstacles is None else set(obstacles)
+    neighbors = {point}
+    yield neighbors
+    exclude.update(neighbors)
+    while neighbors:
+        neighbors = get_all_neighbors(neighbors, excluding=exclude)
+        yield neighbors
+        exclude.update(neighbors)
+
+
+def move_towards(point, targets, obstacles):
+    obstacles = obstacles.difference({point})
+    targets = get_all_neighbors(targets, obstacles).difference(targets)
+    closest = get_closest_point(point, targets, obstacles)
+    if closest:
+        step = step_closer(point, closest, obstacles)
+        if step:
+            return step
+    return point
+
+
+def get_closest_point(point, other_points, obstacles=None):
+    """Finds closest point to `point` in `other_points` cicumnavigating
+    `obstacles`. Ties are broken in reading order."""
+    for neighbors in neighbor_generator(point, obstacles):
+        intersection = neighbors.intersection(other_points)
+        if intersection:
+            return first_in_reading_order(intersection)
+
+
+def step_closer(point, target, obstacles=None):
+    """Returns the next point to move to in order to get closer to target."""
+    obstacles = set() if obstacles is None else set(obstacles)
+    points_to_move_to = get_neighbors(point, excluding=obstacles)
+    points_to_move_to.add(point)
+    for neighbors in neighbor_generator(target, obstacles):
+        intersection = neighbors.intersection(points_to_move_to)
+        if intersection:
+            return first_in_reading_order(intersection)
+
+
+if __name__ == '__main__':
+    game = Game.from_file('input_15.txt')
+    game.run()
+    print(game)
+    print(f'The outcome is: {game.outcome()}')
---
+++ test_15.py
@@ -0,0 +1,266 @@
+#!/usr/bin/env python3
+"""
+2018-12-28 19:15:27
+@author: Paul Reiter
+"""
+from day_15 import (Point, Unit, Game, position_of_symbols_in_string,
+                    get_bounding_box, grid_generator, sort_in_reading_order,
+                    first_in_reading_order, update_dict, get_neighbors,
+                    get_all_neighbors, neighbor_generator, move_towards,
+                    get_closest_point, step_closer)
+
+
+def test_game_str():
+    game = Game(MOVEMENT_TEST[0])
+    assert str(game) == MOVEMENT_TEST[0]
+
+
+def test_game_movement():
+    game = Game(MOVEMENT_TEST[0])
+    rounds = 0
+    for test_string in MOVEMENT_TEST[1:]:
+        game.run_round()
+        assert str(game) == test_string
+        rounds += 1
+        assert game.rounds_completed == rounds
+
+
+def test_game_limited_rounds():
+    game = Game(GAME_TEST[0])
+    for rounds in range(1, 48):
+        game.run_round()
+        assert game.rounds_completed == rounds
+        if rounds in GAME_TEST:
+            assert str(game) == GAME_TEST[rounds]
+
+
+def test_game_open_ended():
+    game = Game(GAME_TEST[0])
+    game.run()
+    assert game.rounds_completed == 47
+    assert sum(unit.hp for unit in game.units.values()) == 590
+    assert game.outcome() == 27730
+
+
+def test_game_several_outcomes():
+    for game, outcome in TEST_OUTCOMES:
+        game = Game(game)
+        game.run()
+        assert game.outcome() == outcome
+
+
+def test_position_of_symbols_in_string():
+    teststring = 'abb\naac\n'
+    reference = {'a': {Point(0, 0), Point(0, 1), Point(1, 1)},
+                 'b': {Point(1, 0), Point(2, 0)},
+                 'c': {Point(2, 1)}}
+    assert position_of_symbols_in_string(teststring) == reference
+
+
+def test_get_bounding_box():
+    points = {Point(2, 0), Point(0, 2), Point(4, 2), Point(2, 3)}
+    assert get_bounding_box(points) == (Point(0, 0), Point(4, 3))
+
+
+def test_grid_generator():
+    reference = [Point(0, 0), Point(1, 0), Point(0, 1), Point(1, 1)]
+    for point, ref in zip(grid_generator(Point(0, 0), Point(1, 1)), reference):
+        assert point == ref
+
+
+def test_sort_in_reading_order():
+    test_points = (Point(1, 2), Point(3, 1), Point(2, 1))
+    assert sort_in_reading_order(test_points) == [test_points[2],
+                                                  test_points[1],
+                                                  test_points[0]]
+
+
+def test_get_neighbors():
+    point = Point(0, 0)
+    excluding = set((Point(1, 0), ))
+    assert (get_neighbors(point, excluding) ==
+            set((Point(-1, 0), Point(0, 1), Point(0, -1))))
+
+
+def test_get_all_neighbors():
+    points = set((Point(0, 0), Point(1, 0)))
+    excluding = set((Point(2, 0), ))
+    assert (get_all_neighbors(points, excluding) ==
+            set((Point(-1, 0), Point(0, 1), Point(x=0, y=0), Point(x=0, y=-1),
+                 Point(x=1, y=0), Point(x=1, y=-1), Point(x=1, y=1))))
+
+
+def test_neighbor_generator():
+    point = Point(0, 0)
+    obstacles = {Point(x, 1) for x in range(-3, 3+1)}
+    gen = neighbor_generator(point, obstacles)
+    assert next(gen) == {point}
+    assert next(gen) == {Point(1, 0), Point(0, -1), Point(-1, 0)}
+    assert next(gen) == {Point(2, 0), Point(1, -1), Point(0, -2),
+                         Point(-1, -1), Point(-2, 0)}
+
+
+def test_get_closest_point():
+    point = Point(0, 0)
+    obstacles = set((Point(2, 0), Point(2, 1), Point(2, -1)))
+    other_points = set((Point(3, 0), Point(4, 0)))
+    assert get_closest_point(point, other_points, obstacles) == Point(3, 0)
+
+
+def test_get_closest_point_same_point():
+    point = Point(0, 0)
+    obstacles = set((Point(2, 0), Point(2, 1), Point(2, -1)))
+    other_points = set((Point(0, 0), Point(4, 0)))
+    assert get_closest_point(point, other_points, obstacles) == Point(0, 0)
+
+
+MOVEMENT_TEST = ['#########\n'
+                 '#G..G..G#\n'
+                 '#.......#\n'
+                 '#.......#\n'
+                 '#G..E..G#\n'
+                 '#.......#\n'
+                 '#.......#\n'
+                 '#G..G..G#\n'
+                 '#########',
+                 '#########\n'
+                 '#.G...G.#\n'
+                 '#...G...#\n'
+                 '#...E..G#\n'
+                 '#.G.....#\n'
+                 '#.......#\n'
+                 '#G..G..G#\n'
+                 '#.......#\n'
+                 '#########',
+                 '#########\n'
+                 '#..G.G..#\n'
+                 '#...G...#\n'
+                 '#.G.E.G.#\n'
+                 '#.......#\n'
+                 '#G..G..G#\n'
+                 '#.......#\n'
+                 '#.......#\n'
+                 '#########',
+                 '#########\n'
+                 '#.......#\n'
+                 '#..GGG..#\n'
+                 '#..GEG..#\n'
+                 '#G..G...#\n'
+                 '#......G#\n'
+                 '#.......#\n'
+                 '#.......#\n'
+                 '#########']
+
+
+GAME_TEST = \
+    {0: ('#######\n'
+         '#.G...#\n'
+         '#...EG#\n'
+         '#.#.#G#\n'
+         '#..G#E#\n'
+         '#.....#\n'
+         '#######'),
+     1: ('#######\n'
+         '#..G..#\n'
+         '#...EG#\n'
+         '#.#G#G#\n'
+         '#...#E#\n'
+         '#.....#\n'
+         '#######'),
+     2: ('#######\n'
+         '#...G.#\n'
+         '#..GEG#\n'
+         '#.#.#G#\n'
+         '#...#E#\n'
+         '#.....#\n'
+         '#######'),
+     23: ('#######\n'
+          '#...G.#\n'
+          '#..G.G#\n'
+          '#.#.#G#\n'
+          '#...#E#\n'
+          '#.....#\n'
+          '#######'),
+     24: ('#######\n'
+          '#..G..#\n'
+          '#...G.#\n'
+          '#.#G#G#\n'
+          '#...#E#\n'
+          '#.....#\n'
+          '#######'),
+     25: ('#######\n'
+          '#.G...#\n'
+          '#..G..#\n'
+          '#.#.#G#\n'
+          '#..G#E#\n'
+          '#.....#\n'
+          '#######'),
+     26: ('#######\n'
+          '#G....#\n'
+          '#.G...#\n'
+          '#.#.#G#\n'
+          '#...#E#\n'
+          '#..G..#\n'
+          '#######'),
+     27: ('#######\n'
+          '#G....#\n'
+          '#.G...#\n'
+          '#.#.#G#\n'
+          '#...#E#\n'
+          '#...G.#\n'
+          '#######'),
+     28: ('#######\n'
+          '#G....#\n'
+          '#.G...#\n'
+          '#.#.#G#\n'
+          '#...#E#\n'
+          '#....G#\n'
+          '#######'),
+     47: ('#######\n'
+          '#G....#\n'
+          '#.G...#\n'
+          '#.#.#G#\n'
+          '#...#.#\n'
+          '#....G#\n'
+          '#######')}
+
+
+TEST_OUTCOMES = [
+                 (('#######\n'
+                   '#G..#E#\n'
+                   '#E#E.E#\n'
+                   '#G.##.#\n'
+                   '#...#E#\n'
+                   '#...E.#\n'
+                   '#######'), 36334),
+                 (('#######\n'
+                   '#E..EG#\n'
+                   '#.#G.E#\n'
+                   '#E.##E#\n'
+                   '#G..#.#\n'
+                   '#..E#.#\n'
+                   '#######'), 39514),
+                 (('#######\n'
+                   '#E.G#.#\n'
+                   '#.#G..#\n'
+                   '#G.#.G#\n'
+                   '#G..#.#\n'
+                   '#...E.#\n'
+                   '#######'), 27755),
+                 (('#######\n'
+                   '#.E...#\n'
+                   '#.#..G#\n'
+                   '#.###.#\n'
+                   '#E#G#G#\n'
+                   '#...#G#\n'
+                   '#######'), 28944),
+                 (('#########\n'
+                   '#G......#\n'
+                   '#.E.#...#\n'
+                   '#..##..G#\n'
+                   '#...##..#\n'
+                   '#...#...#\n'
+                   '#.G...G.#\n'
+                   '#.....G.#\n'
+                   '#########'), 18740),
+                 ]

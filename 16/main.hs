d14d65065464f1da28efe695c296ce72926d8d5f
Paul Reiter <reiter.paul@gmail.com>
2022-12-06 21:00:33 +0100

    Add solution for day 16 part 2

--- 16/main.hs
+++ 16/main.hs
@@ -1,8 +1,12 @@
 {-# LANGUAGE OverloadedStrings #-}
-import Data.Text qualified as T
+import Data.Text (Text(..), splitOn, unpack, strip)
 import Data.Text.IO qualified as IO
 import Data.Sequence (Seq(..), fromList, update, index)
 import Data.Bits ((.&.), (.|.))
+import Data.Map (Map(..), lookup, empty)
+import Data.Map qualified as M
+import Data.Set qualified as S
+import Control.Monad (foldM)
 
 type Register = Seq Int
 type Opcode = Int
@@ -10,6 +14,8 @@ type InsOut = (Int, Int, Int)
 type Instruction = (Opcode, InsOut)
 type Operation = InsOut -> Register -> Register
 type Example = (Register, Instruction, Register)
+type FunctionMap = Map Int Operation
+type FunctionIndexMap = Map Int Int
 
 addr :: Operation
 addr (a, b, c) r = update c (index r a + index r b) r
@@ -32,23 +38,55 @@ eqrr (a, b, c) r = update c (if index r a == index r b then 1 else 0) r
 functions :: [Operation]
 functions = [addr, addi, mulr, muli, banr, bani, borr, bori, setr, seti, gtir, gtri, gtrr, eqir, eqri, eqrr]
 
-parseExample :: T.Text -> Example
+parseExample :: Text -> Example
 parseExample t = (beforeRegister, (opcode, (a, b, c)), afterRegister)
-    where (before:middle:after:_) = T.splitOn "\n" t
-          parseRegister = fromList . read . T.unpack . last . T.splitOn ": "
+    where (before:middle:after:_) = splitOn "\n" t
+          parseRegister = fromList . read . unpack . last . splitOn ": "
           beforeRegister = parseRegister before
           afterRegister = parseRegister after
-          (opcode:a:b:c:_) = map (read . T.unpack ) (T.splitOn " " middle)
+          (opcode:a:b:c:_) = map (read . unpack ) (splitOn " " middle)
 
-parseInput :: T.Text -> ([Example], a)
-parseInput text = (map parseExample (T.splitOn "\n\n" firstPart), undefined)
-    where (firstPart:secondPart:_) = T.splitOn "\n\n\n" text
+parseInput :: Text -> ([Example], [Instruction])
+parseInput text = (map parseExample (splitOn "\n\n" firstPart), program)
+    where (firstPart:secondPart:_) = splitOn "\n\n\n\n" text
+          program = map parseInstruction (splitOn "\n" (strip secondPart))
 
-howManyFitExample :: Example -> Int
-howManyFitExample (input, (_, io), output) = sum $ map (fromEnum . (\fun -> fun io input == output)) functions
+parseInstruction :: Text -> Instruction
+parseInstruction t = (opcode, (a, b, c))
+    where (opcode:a:b:c:_) = map (read . unpack) (splitOn " " t)
+
+howManyFunctionsFitExample :: Example -> Int
+howManyFunctionsFitExample e = sum $ fmap (fromEnum . fitsExample e) functions
+
+fitsExample :: Example -> Operation -> Bool
+fitsExample (input, (_, insOut), output) fun = fun insOut input == output
+
+runInstruction :: FunctionMap -> Register -> Instruction -> Maybe Register
+runInstruction fm r (opcode, insOut) = case M.lookup opcode fm of
+        Nothing -> Nothing
+        Just fun -> Just (fun insOut r)
+
+run :: FunctionMap -> [Instruction] -> Maybe Register
+run fm = foldM (runInstruction fm) (fromList [0, 0, 0, 0])
+
+findFunctionIndexMap :: Map Int (S.Set Int) -> FunctionIndexMap -> FunctionIndexMap
+findFunctionIndexMap opm fim
+    | M.null opm = fim
+    | otherwise = findFunctionIndexMap newOpm newFim
+        where uniqueFunctions = filter (\(_, funIdxSet) -> S.size funIdxSet == 1) (M.toList opm)
+              newFim = foldr (\(opcode, funIdxSet) acc -> M.insert opcode (head $ S.toList funIdxSet) acc) fim uniqueFunctions
+              uniqueFunctionIdxs = S.fromList [head $ S.toList idx | (_, idx) <- uniqueFunctions]
+              removedFunctionIdxs = [(opcode, S.difference funIdxSet uniqueFunctionIdxs) | (opcode, funIdxSet) <- M.toList opm]
+              newOpm = (M.fromList . filter (\(_, s) -> not $ S.null s)) removedFunctionIdxs
+
+enumerate = zip [0..]
 
 main :: IO ()
 main = do
-    (examples, _) <- parseInput <$> IO.readFile "input.txt"
-    print examples
-    print $ sum $ map (fromEnum . (>=3) . howManyFitExample) examples
+    (examples, program) <- parseInput <$> IO.readFile "input.txt"
+    print $ sum $ map (fromEnum . (>=3) . howManyFunctionsFitExample) examples
+    let allOpcodesAllFunctions = concat [[(opcode, idx) | (idx, fun) <- enumerate functions, fun insOut input == output] | e@(input, (opcode, insOut), output) <- examples]
+    let allOpcodesAllFunctionsMap = foldr (\(opcode, idx) acc -> M.insertWith S.union opcode (S.singleton idx) acc) empty allOpcodesAllFunctions
+    let functionIndexMap = findFunctionIndexMap allOpcodesAllFunctionsMap M.empty
+    let functionMap = M.fromList [(opcode, functions!!funIdx) | (opcode, funIdx) <- M.toList functionIndexMap]
+    print $ run functionMap program
